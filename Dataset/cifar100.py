c7db0977ec13406dbf06f7a6256250d825a8e2c0
Daniele Genta <d.genta@jetop.com>
2020-05-13 14:34:45 +0200

    cifar100 handler

---
+++ Dataset/cifar100.py
@@ -0,0 +1,88 @@
+# ref: https://github.com/pytorch/vision/blob/master/torchvision/datasets/cifar.py
+# https://pytorch.org/docs/stable/_modules/torchvision/datasets/cifar.html#CIFAR10
+from PIL import Image
+import os
+import os.path
+import numpy as np
+import pickle
+from torchvision.datasets import VisionDataset
+
+	class CIFAR100(VisionDataset):
+	    """`CIFAR100 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
+	    This is a subclass of the `CIFAR10` Dataset.
+	    """
+	    base_folder = 'cifar-100-python'
+	    train_file = 'train'
+	    test_file = 'test'
+	    meta_file = 'meta'
+
+    def __init__(self, root, split = 'train', transform = None):
+
+        super(CIFAR100, self).__init__(root, transform=transform)
+
+        self.split = split
+        if split == 'train':
+            filename = self.train_file
+        else:
+            filename = self.test_file
+
+        if download:
+            self.download()
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+        data_path = os.path.join(self.root, self.base_folder, filename)
+        self.data = []
+        self.targets = []
+
+         with open(data_path, 'rb') as f:
+            entry = pickle.load(f, encoding='latin1')
+            self.data = entry['data']
+            self.labels = entry['fine_labels']
+        
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # Convert to HWC
+        
+        self.labels = np.array(self.labels)
+
+        self._load_meta()
+
+    def _load_meta(self):
+         meta_path = os.path.join(self.root, self.base_folder, self.meta_filename)
+        with open(meta_path, 'rb') as f:
+            meta = pickle.load(f, encoding='latin1')
+            self.label_names = meta['fine_label_names']
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img) # Return a PIL image
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+    def _check_integrity(self):
+        root = self.root
+        for fentry in (self.train_list + self.test_list):
+            filename, md5 = fentry[0], fentry[1]
+            fpath = os.path.join(root, self.base_folder, filename)
+            if not check_integrity(fpath, md5):
+                return False
+        return True
+
+
