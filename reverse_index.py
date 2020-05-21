c5fb3388b392237d3a0953f6e03da2e5d896e30f
Danver97 <chri.pae@gmail.com>
2020-05-21 15:34:46 +0200

    Added reverse index class

---
+++ reverse_index.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import torch
+
+__all__ = ['ReverseIndex']
+
+class ReverseIndex():
+
+    def __init__(self, dataset, splits, device='cuda'):
+
+        self.df = pd.DataFrame(columns=['group', 'labels'])
+
+        for k in splits.keys():
+            labels = list(dataset.df.loc[splits[k]['train'],'labels'].value_counts().index)
+            group = [k for i in range(len(labels))]
+            data = pd.DataFrame(np.array([group, labels]).T, columns=['group', 'labels'])
+            self.df = self.df.append(data, ignore_index=True)
+
+        self.df['nodes'] = self.df.index
+        self.device = device
+    
+    def _changeIndex(self, reverse_index, column):
+        reverse_index = reverse_index.set_index(column)
+        reverse_index[column] = reverse_index.index
+        return reverse_index
+
+    def getLabels(self, outputs):
+        outs = outputs.cpu().numpy()
+        reverse_index = self._changeIndex(self.df, 'nodes')
+        labels = reverse_index.loc[outs, 'labels']
+
+        labels = torch.tensor(list(labels))
+        return labels.to(self.device)
+
+    def getNodes(self, labels):
+        labels = labels.cpu().numpy()
+        reverse_index = self._changeIndex(self.df, 'labels')
+        nodes = reverse_index.loc[labels, 'nodes']
+
+        nodes = torch.tensor(list(nodes))
+        return nodes.to(self.device)
+
+    def getGroups(self, distinct=True):
+        return self.df['group'].value_counts().index.sort_values()
+    
+    def getLabelsOfGroup(self, group):
+        return self.df.loc[self.df['group'] == group, 'labels']
+
