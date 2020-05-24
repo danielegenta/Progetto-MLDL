adcc7ffbc0f2ba99318d006f968a2b05a9333ed9
Daniele Genta <d.genta@jetop.com>
2020-05-24 23:56:27 +0200

    prima o poi andrà

--- lwf_model.py
+++ lwf_model.py
@@ -86,7 +86,7 @@ class LWF(nn.Module):
   def update_representation(self, dataset, new_classes):
     #previous_model = copy.deepcopy(self)
     #previous_model.to(self.DEVICE)
-    self.to(DEVICE)
+    self.to(self.DEVICE)
 
     # 3 - increment classes
     #          (add output nodes)
