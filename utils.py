a33289cbd0399f657edfffd8e97efc207253ded2
Daniele Genta <d.genta@jetop.com>
2020-05-25 15:34:40 +0200

    upd

--- utils.py
+++ utils.py
@@ -7,19 +7,21 @@ import torch.optim as optim
 # These are the default iCaRL hyper-parameters
 def getHyperparams():
 	dictHyperparams = {
-		"LR": [2],
-		"MOMENTUM": [0.9],
-		"WEIGHT_DECAY": [1e-5],
-		"NUM_EPOCHS": [70],
+		"LR": 2,
+		"MOMENTUM": 0.9,
+		"WEIGHT_DECAY": 1e-5,
+		"NUM_EPOCHS": 70,
 		"MILESTONES": [49, 63],
-		"BATCH-SIZE": [128],
+		"BATCH-SIZE": 128,
 		"DEVICE": 'cuda',
-		"GAMMA": [0.2],
-		"SEED": [30]
+		"GAMMA": 0.2,
+		"SEED": 30,
+		"LOG_FREQUENCY": 10,
+		"NUM_CLASSES": 100
 	}
 	return dictHyperparams
 
-def getOptimizerScheduler(LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA):
+def getOptimizerScheduler(LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA, parameters_to_optimize):
 	optimizer = optim.SGD(parameters_to_optimize, lr=LR, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
 	scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=MILESTONES, gamma=GAMMA, last_epoch=-1) 
 	return optimizer, scheduler
@@ -40,7 +42,7 @@ def getTransfomrations():
 	return train_transform, eval_transform
 
 def getLossCriterion():
-	criterion = nn.BCEWithLogitsLoss() # for classification: Cross Entropy
+	criterion = nn.CrossEntropyLoss() # for classification: Cross Entropy
 	return criterion
 
 def plotAccuracyTrend():
