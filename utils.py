7e6975ec791a0ed4379f1b23794cd2362f4a20e0
Daniele Genta <d.genta@jetop.com>
2020-05-25 15:07:35 +0200

    init common utils function

---
+++ utils.py
@@ -0,0 +1,46 @@
+from torchvision import transforms
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+
+# These are the default iCaRL hyper-parameters
+def getHyperparams():
+	dictHyperparams = {
+		"LR": 2,
+		"MOMENTUM": 0.9,
+		"WEIGHT_DECAY": 1e-5,
+		"NUM_EPOCHS": 70,
+		"MILESTONES": [49, 63]
+		"BATCH-SIZE": 128,
+		"DEVICE": 'cuda',
+		"GAMMA": 0.2,
+		"DATA_DIR": 'DATA'
+		"SEED": 30
+	}
+	return dictHyperparams
+
+def getOptimizerScheduler(LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA):
+	optimizer = optim.SGD(parameters_to_optimize, lr=LR, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
+	scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=MILESTONES, gamma=GAMMA, last_epoch=-1) 
+	return optimizer, scheduler
+
+def getTransfomrations():
+	# Define transforms for training phase
+	train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), # Randomly flip the image with probability of 0.5
+	                                      transforms.Pad(4), # Add padding
+	                                      transforms.RandomCrop(32),# Crops a random squares of the image
+	                                      transforms.ToTensor(), # Turn PIL Image to torch.Tensor
+	                                      transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) 
+	])
+	# Define transforms for the evaluation phase
+	eval_transform = transforms.Compose([
+	                                      transforms.ToTensor(),
+	                                      transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) 
+	])
+	return train_transform, eval_transform
+
+def getLossCriterion():
+	criterion = nn.BCEWithLogitsLoss() # for classification: Cross Entropy
+	return criterion
+
