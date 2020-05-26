e25080bd5249731ce7e3c91f8e025de13b82874e
Daniele Genta <d.genta@jetop.com>
2020-05-26 16:31:36 +0200

    upd

--- lwf_model.py
+++ lwf_model.py
@@ -12,26 +12,25 @@ from Cifar100.resnet import resnet32
 from Cifar100.Dataset.cifar100 import CIFAR100
 import copy
 
-# Hyper Parameters
-# ...
+from Cifar100 import utils
 
 # feature size: ???
 # n_classes: 10 => 100
 class LWF(nn.Module):
-  def __init__(self, feature_size, n_classes, BATCH_SIZE, WEIGHT_DECAY, LR, GAMMA, NUM_EPOCHS, DEVICE,MILESTONES):
+  def __init__(self, feature_size, n_classes, BATCH_SIZE, WEIGHT_DECAY, LR, GAMMA, NUM_EPOCHS, DEVICE,MILESTONES, reverse_index = None):
     super(LWF, self).__init__()
     self.feature_extractor = resnet32()
 
     self.feature_extractor.fc = nn.Linear(self.feature_extractor.fc.in_features,feature_size)
-    self.bn = nn.BatchNorm1d(feature_size, momentum=0.01)
+    self.bn = nn.BatchNorm1d(feature_size, momentum=MOMENTUM)
     self.ReLU = nn.ReLU()
     self.fc = nn.Linear(feature_size, n_classes, bias = False)
 
     self.n_classes = n_classes
     self.n_known = 0
 
-    self.cls_loss = nn.BCEWithLogitsLoss(reduction = 'mean')
-    self.dist_loss = nn.BCEWithLogitsLoss(reduction = 'mean')
+    self.cls_loss = utils.getLossCriterion()
+    self.dist_loss = utils.getLossCriterion()
     
     self.BATCH_SIZE = BATCH_SIZE
     self.WEIGHT_DECAY  = WEIGHT_DECAY
@@ -39,10 +38,10 @@ class LWF(nn.Module):
     self.GAMMA = GAMMA # this allow LR to become 1/5 LR after MILESTONES epochs
     self.NUM_EPOCHS = NUM_EPOCHS
     self.DEVICE = DEVICE
-
     self.MILESTONES = MILESTONES # when the LR decreases, according to icarl
-    self.optimizer = optim.SGD(self.parameters(), lr=self.LR, weight_decay=self.WEIGHT_DECAY)
-    self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=MILESTONES, gamma=self.GAMMA)
+    
+    self.optimizer, self.scheduler = utils.utils.getOptimizerScheduler(LR, MOMENTUM, WEIGHT_DECAY, MILESTONES, GAMMA, self.parameters)
+    self.reverse_index=reverse_index
 
     
   def forward(self, x):
@@ -76,13 +75,6 @@ class LWF(nn.Module):
         _, preds = torch.max(torch.softmax(self.forward(images), dim=1), dim=1, keepdim=False)
         return preds    
 
-
-  def _one_hot_encode(self, labels, dtype=None, device=None):
-    enconded = torch.zeros(self.n_classes, len(labels), dtype=dtype, device=device)
-    for i, l in enumerate(labels):
-      enconded[i, l] = 1
-    return enconded
-
   def update_representation(self, dataset, new_classes):
     #previous_model = copy.deepcopy(self)
     #previous_model.to(self.DEVICE)
@@ -97,46 +89,47 @@ class LWF(nn.Module):
     loader = DataLoader(dataset, batch_size=self.BATCH_SIZE,shuffle=True, num_workers=4, drop_last = True)
 
     # 5 - store network outputs with pre-update parameters => q
-    q = torch.zeros(len(dataset), self.n_classes)
+    q = torch.zeros(len(dataset), self.n_classes).to('cuda')
     for indices, images, labels in loader:
-        images = Variable(images).to(self.DEVICE)
+        images = images.to(self.DEVICE)
         indices = indices.to(self.DEVICE)
-        g = nn.functional.sigmoid(self.forward(images))
+        g = torch.sigmoid(self.forward(images))
         q[indices] = g.data
-    q = Variable(q).to(self.DEVICE)
 
     optimizer = self.optimizer
 
-
+    cudnn.benchmark # Calling this optimizes runtime
     for epoch in range(NUM_EPOCHS):
+        print("NUM_EPOCHS: ",epoch,"/", self.NUM_EPOCHS)
         for indices, images, labels in loader:
             # Bring data over the device of choice
-            images = Variable(images).to(self.DEVICE)
+            images = images.to(self.DEVICE)
             #labels = self._one_hot_encode(labels, device=self.DEVICE)
-            labels = Variable(labels).to(self.DEVICE)
+            labels = labels.to(self.DEVICE)
             indices = indices.to(self.DEVICE)
-
+            labels = utils._one_hot_encode(labels, reverse_index, device=self.DEVICE)
+            
             # PyTorch, by default, accumulates gradients after each backward pass
             # We need to manually set the gradients to zero before starting a new iteration
             optimizer.zero_grad() # Zero-ing the gradients
 
             # Forward pass to the network
-            outputs = self.forward(images)
+            g = self.forward(images)
 
             # Classification loss for new classes
-            labels_onehot = nn.functional.one_hot(labels,n_classes)
-            labels_onehot = labels_onehot.type_as(outputs)
-            loss = sum(self.cls_loss(g[:,y], labels_onehot[:,y]) for y in range(self.n_known, self.n_classes))
+            loss = sum(self.cls_loss(g[:,y], labels[:,y]) for y in range(self.n_known, self.n_classes))
 
             # Distillation loss for old classes
             # Distilation loss for old classes
             if self.n_known > 0:
-                g = F.sigmoid(g)
+                #g = F.sigmoid(g)
                 q_i = q[indices]
                 # to check!
-                for y in range(0,len(self.exemplar_sets)):
-                    dist_loss += self.dist_loss(g[:,y],q_i[:,y])
+                distillation = sum(self.dist_loss(g[:,y],q_i[:,y]) for y in range(0, self.n_known))
+
                 loss += dist_loss
 
             loss.backward()
             optimizer.step()
+        print("LOSS: ",loss)
+
