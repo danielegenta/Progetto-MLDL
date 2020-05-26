d50ee5727790718780d69e692d0d315eb40945c1
Daniele Genta <d.genta@jetop.com>
2020-05-26 23:45:55 +0200

    upd

--- lwf_model.py
+++ lwf_model.py
@@ -62,7 +62,7 @@ class LWF(nn.Module):
         out_features = self.fc.out_features
         weight = self.fc.weight.data
 
-        self.fc = nn.Linear(in_features, out_features+n, bias=False)
+        self.fc = nn.Linear(in_features, out_features+n)
         self.fc.weight.data[:out_features] = weight
         self.n_classes += n
 
