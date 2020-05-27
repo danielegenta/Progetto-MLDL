b65bd624fe7e6add257783946c10784cc778bce1
Daniele Genta <d.genta@jetop.com>
2020-05-27 22:47:25 +0200

    upd

--- icarl_model.py
+++ icarl_model.py
@@ -332,7 +332,10 @@ class ICaRL(nn.Module):
             print(images.size())
 
             #loss = sum(self.cls_loss(g[:,y], labels[:,y]) for y in range(self.n_known, self.n_classes))
-            classes_with_exemplars = self.n_classes * 2
+            if self.n_known > 0:
+              classes_with_exemplars = self.n_classes + len(exemplar_sets)
+            else:
+              classes_with_exemplars = self.n_classes
             labels_one_hot = utils._one_hot_encode(labels,classes_with_exemplars, self.reverse_index, device=self.DEVICE)
             labels_one_hot.type_as(outputs)
 
