1cc729bf606541430a0be7fca8345250b0480336
Daniele Genta <d.genta@jetop.com>
2020-05-27 23:01:11 +0200

    upd

--- icarl_model.py
+++ icarl_model.py
@@ -327,7 +327,7 @@ class ICaRL(nn.Module):
             outputs = net(images)
 
             #loss = sum(self.cls_loss(g[:,y], labels[:,y]) for y in range(self.n_known, self.n_classes))
-            labels_one_hot = utils._one_hot_encode(labels,n_classes, self.reverse_index, device=self.DEVICE)
+            labels_one_hot = utils._one_hot_encode(labels,self.n_classes, self.reverse_index, device=self.DEVICE)
             labels_one_hot.type_as(outputs)
 
             # test
