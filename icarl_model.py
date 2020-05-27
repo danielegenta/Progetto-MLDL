59be671e0a0fd366d96d05fcd0a7f33eaf7ae390
Daniele Genta <d.genta@jetop.com>
2020-05-27 19:20:15 +0200

    upd

--- icarl_model.py
+++ icarl_model.py
@@ -315,7 +315,7 @@ class ICaRL(nn.Module):
             optimizer.zero_grad() # Zero-ing the gradients
 
             # Forward pass to the network
-            outputs = self.forward(images)
+            outputs = net(images)
 
             #loss = sum(self.cls_loss(g[:,y], labels[:,y]) for y in range(self.n_known, self.n_classes))
             labels_one_hot = utils._one_hot_encode(labels,self.n_classes, self.reverse_index, device=self.DEVICE)
