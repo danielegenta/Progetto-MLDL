dd660e4c8a63136159b9ce392deca52d09ae125a
Daniele Genta <d.genta@jetop.com>
2020-05-28 09:26:25 +0200

    upd

--- icarl_model.py
+++ icarl_model.py
@@ -242,8 +242,8 @@ class ICaRL(nn.Module):
     print(len(self.exemplar_sets))
     for exemplar_set in self.exemplar_sets: #for each class and exemplar set for that class
         for exemplar in exemplar_set:
-            exemplar = Image.fromarray(exemplar.numpy()) # Return a PIL image
-            aus_dataset.append((exemplar.squeeze(), index)) # nb i do not append the label yet a simple index, 0 is just a placeholder
+            exemplar = Image.fromarray(exemplar) # Return a PIL image
+            aus_dataset.append((exemplar, index)) # nb i do not append the label yet a simple index, 0 is just a placeholder
         index += 1
 
     return aus_dataset 
