56888f9296fd6b74b7a9c009d59afc30f81e592b
Daniele Genta <d.genta@jetop.com>
2020-05-28 09:02:22 +0200

    upd

--- icarl_model.py
+++ icarl_model.py
@@ -314,9 +314,9 @@ class ICaRL(nn.Module):
             # Forward pass to the network
             outputs = net(images)
             if self.n_known > 0:
-              outputs = old_net(images)
+              print(images)
 
-            print(outputs.size())
+            #print(outputs.size())
 
             labels_one_hot = utils._one_hot_encode(labels, self.n_classes, self.reverse_index, device=self.DEVICE)
             labels_one_hot.type_as(outputs)
