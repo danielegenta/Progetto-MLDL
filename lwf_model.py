71dae290d73814ce5621c137018dd0362c1508b8
Danver97 <chri.pae@gmail.com>
2020-06-05 10:05:38 +0200

    Added support for different losses in LwF

--- lwf_model.py
+++ lwf_model.py
@@ -15,10 +15,27 @@ import gc
 
 from Cifar100 import utils
 
+def auto_loss_rebalancing(n_known, n_classes, loss_type):
+  alpha = n_known/n_classes 
+
+  if loss_type == 'class':
+    return 1-alpha
+  return alpha
+
+def get_rebalancing(rebalancing=None):
+  if rebalancing is None:
+    return lambda n_known, n_classes, loss_type: 1
+  if rebalancing in ['auto', 'AUTO']:
+    return auto_loss_rebalancing
+  if callable(rebalancing):
+    return rebalancing
+
+
 # feature size: 2048 (currently)
 # n_classes: 10 => 100
 class LWF(nn.Module):
-  def __init__(self, feature_size, n_classes, BATCH_SIZE, WEIGHT_DECAY, LR, GAMMA, NUM_EPOCHS, DEVICE,MILESTONES,MOMENTUM, reverse_index = None):
+  def __init__(self, feature_size, n_classes, BATCH_SIZE, WEIGHT_DECAY, LR, GAMMA, NUM_EPOCHS, DEVICE,MILESTONES,MOMENTUM, \
+      reverse_index = None, class_loss_criterion='bce', dist_loss_criterion='bce', loss_rebalancing='auto', lambda0=1):
     super(LWF, self).__init__()
     self.feature_extractor = resnet32()
     self.feature_extractor.fc = nn.Linear(self.feature_extractor.fc.in_features,feature_size)
@@ -40,6 +57,8 @@ class LWF(nn.Module):
     self.MOMENTUM = MOMENTUM
     
     self.reverse_index=reverse_index
+    
+    self.class_loss, self.dist_loss = self.build_loss(class_loss_criterion, dist_loss_criterion, loss_rebalancing, lambda0=lambda0)
 
     self.optimizer, self.scheduler = utils.getOptimizerScheduler(self.LR, self.MOMENTUM, self.WEIGHT_DECAY, self.MILESTONES, self.GAMMA, self.parameters())
 
@@ -124,31 +143,105 @@ class LWF(nn.Module):
             # Forward pass to the network
             outputs = self.forward(images)
 
-            labels_one_hot = utils._one_hot_encode(labels,self.n_classes, self.reverse_index, device=self.DEVICE)
-            # test
-            #labels_one_hot = nn.functional.one_hot(labels, self.n_classes)
-            labels_one_hot.type_as(outputs)
+            # Loss = only classification on new classes
+            loss = self.class_loss(outputs, labels, col_start=self.n_known)
+            class_loss = loss.item() # Used for logging for debugging purposes
 
-            # Classification loss for new classes            
-            if self.n_known == 0:
-                loss = criterion(outputs, labels_one_hot)
-            elif self.n_known > 0:
-            
-                labels_one_hot = labels_one_hot.type_as(outputs)[:,self.n_known:]
-                out_old = Variable(torch.sigmoid(old_net(images))[:,:self.n_known],requires_grad = False)
-                
-                #[outputold, onehot_new]
-                target = torch.cat((out_old, labels_one_hot),dim=1)
-                loss = criterion(outputs,target)
+            # Distilation loss for old classes, class loss on new classes
+            dist_loss = None
+            if len(self.exemplar_sets) > 0:
+              out_old = torch.sigmoid(old_net(images))
+              dist_loss = self.dist_loss(outputs, out_old, col_end=self.n_known)
+              loss += dist_loss
 
             loss.backward()
             optimizer.step()
 
         scheduler.step()
-        print("LOSS: ",loss)
+        print("LOSS: ", loss.item(), 'class loss', class_loss, 'dist loss', dist_loss.item() if dist_loss is not None else dist_loss)
 
     gc.collect()
     del net
     torch.no_grad()
     torch.cuda.empty_cache()
+
+
+
+  def build_loss(self, class_loss_criterion, dist_loss_criterion, rebalancing=None, lambda0=1):
+    class_loss_func = None
+    dist_loss_func = None
+
+    if class_loss_criterion in ['l2', 'L2']:
+      class_loss_func = self.l2_class_loss
+    elif class_loss_criterion in ['bce', 'BCE']:
+      class_loss_func = self.bce_class_loss
+    elif class_loss_criterion in ['ce', 'CE']:
+      class_loss_func = self.ce_class_loss
+
+    if dist_loss_criterion in ['l2', 'L2']:
+      dist_loss_func = self.l2_dist_loss
+    elif dist_loss_criterion in ['bce', 'BCE']:
+      dist_loss_func = self.bce_dist_loss
+    elif dist_loss_criterion in ['ce', 'CE']:
+      dist_loss_func = self.ce_dist_loss
+
+    rebalancing = get_rebalancing(rebalancing)
+    
+    def class_loss(outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+      alpha = rebalancing(self.n_known, self.n_classes, 'class')
+      return alpha*class_loss_func(outputs, labels, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+    
+    def dist_loss(outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+      alpha = rebalancing(self.n_known, self.n_classes, 'dist')
+      return lambda0*alpha*dist_loss_func(outputs, labels, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+    
+    return class_loss, dist_loss
+
+  def bce_class_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.bce_loss(outputs, labels, encode=True, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+
+  def bce_dist_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.bce_loss(outputs, labels, encode=False, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+
+  def ce_class_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.ce_loss(outputs, self.reverse_index.getNodes(labels), decode=False, row_start=row_start, row_end=row_end, col_start=None, col_end=col_end)
+    
+  def ce_dist_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.ce_loss(outputs, labels, decode=True, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+
+  def l2_class_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.bce_loss(outputs, labels, encode=True, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+
+  def l2_dist_loss(self, outputs, labels, row_start=None, row_end=None, col_start=None, col_end=None):
+    return self.bce_loss(outputs, labels, encode=False, row_start=row_start, row_end=row_end, col_start=col_start, col_end=col_end)
+
+
+  def bce_loss(self, outputs, labels, encode=False, row_start=None, row_end=None, col_start=None, col_end=None):
+    criterion = nn.BCEWithLogitsLoss(reduction = 'mean')
+
+    if encode:
+      labels = utils._one_hot_encode(labels, self.n_classes, self.reverse_index, device=self.DEVICE)
+      labels = labels.type_as(outputs)
+
+    return criterion(outputs[row_start:row_end, col_start:col_end], labels[row_start:row_end, col_start:col_end])
+
+
+  def ce_loss(self, outputs, labels, decode=False, row_start=None, row_end=None, col_start=None, col_end=None):
+    criterion = nn.CrossEntropyLoss()
+
+    if decode:
+      labels = torch.argmax(labels, dim=1)
+    
+    return criterion(outputs[row_start:row_end, col_start:col_end], labels[row_start:row_end])
+
+
+  def l2_loss(self, outputs, labels, encode=False, row_start=None, row_end=None, col_start=None, col_end=None):
+    criterion = nn.MSELoss(reduction = 'mean')
+    
+    if encode:
+      labels = utils._one_hot_encode(labels, self.n_classes, self.reverse_index, device=self.DEVICE)
+      labels = labels.type_as(outputs)
+    
+    return criterion(outputs[row_start:row_end, col_start:col_end], labels[row_start:row_end, col_start:col_end])
+
 
